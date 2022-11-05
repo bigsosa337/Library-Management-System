2243378774405a91cbe4397f74bef93e3a13ad2a
bigsosa337 <115635392+bigsosa337@users.noreply.github.com>
2022-11-05 16:47:26 +0200

    Added constant files

---
+++ src/library/Options.java
@@ -0,0 +1,18 @@
+package library;
+
+/**
+ * This class was created to make it easier to distinct from the options selected by the users
+ */
+
+public class Options {
+    public static final int ADD_BOOK = 1;
+    public static final int SHOW_ALL_BOOKS = 2;
+    public static final int SHOW_AVAILABLE_BOOKS = 3;
+    public static final int BORROW_BOOK = 4;
+    public static final int RETURN_BOOK = 5;
+    public static final int EXIT = 6;
+
+
+
+
+}
