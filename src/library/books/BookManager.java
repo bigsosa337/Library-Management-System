7b35765cd0e1c18c519350aaeb6d2b7f0ff23fd5
bigsosa337 <115635392+bigsosa337@users.noreply.github.com>
2022-11-05 21:27:59 +0200

    Added book manager

---
+++ src/library/books/BookManager.java
@@ -0,0 +1,28 @@
+package library.books;
+
+public class BookManager {
+    public static void addBook() {
+
+    }
+
+    public static void showAllBooks() {
+
+    }
+
+    public static void showAvailableBooks() {
+
+    }
+
+    public static void borrowBook() {
+
+    }
+
+    public static void returnBook() {
+
+    }
+
+    public static void exit() {
+
+    }
+}
+
