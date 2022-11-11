2f6acba7d1faba9af3008bc40046208cb643a2b0
Andrei <acdnuclear@gmail.com>
2022-11-11 21:24:01 +0200

    Implemented search method

---
+++ src/main/java/library/Search.java
@@ -0,0 +1,75 @@
+package library;
+
+import library.books.Book;
+
+import java.util.Map;
+import java.util.Scanner;
+
+import static library.SearchOptions.*;
+import static library.books.BookManager.bookshelf;
+
+public class Search {
+
+    public static void searchBookByName() {
+        System.out.println("Introdu numele cartii:");
+        Scanner scan = new Scanner(System.in);
+        Map<String, Book> books = bookshelf.getBooks();
+        String bookBorrowName = scan.nextLine();
+        for (Map.Entry<String, Book> entry : books.entrySet()) {
+            if (entry.getValue().getBookName().equals(bookBorrowName)) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+    public static void searchBookByYear() {
+        System.out.println("Introdu anul publicatiei:");
+        Scanner scan = new Scanner(System.in);
+        Map<String, Book> books = bookshelf.getBooks();
+        String bookBorrowName = scan.nextLine();
+        for (Map.Entry<String, Book> entry : books.entrySet()) {
+            if (entry.getValue().getBookPublishDate().equals(bookBorrowName)) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+    public static void searchBookByAuthor() {
+        System.out.println("Introdu numele autorului:");
+        Scanner scan = new Scanner(System.in);
+        Map<String, Book> books = bookshelf.getBooks();
+        String bookBorrowName = scan.nextLine();
+        for (Map.Entry<String, Book> entry : books.entrySet()) {
+            if (entry.getValue().getBookAuthor().equals(bookBorrowName)) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+//    public static void searchBookByYear() {
+//        System.out.println("Introdu data imprumutului: ");
+//        Scanner scan = new Scanner(System.in);
+//        Map<String, Book> books = bookshelf.getBooks();
+//        String bookBorrowName = scan.nextLine();
+//        for (Map.Entry<String, Book> entry : books.entrySet()) {
+//            if (entry.getValue().getBookPublishDate().equals(bookBorrowName)) {
+//                System.out.println(entry.getValue());
+//            }
+//        }
+//    }
+
+    public static void searchBooks() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (CAUTARE_NUME == a) {
+            Search.searchBookByName();
+        } else if (CAUTARE_AUTOR == a) {
+            Search.searchBookByAuthor();
+        } else if (CAUTARE_AN == a) {
+            Search.searchBookByYear();
+        } else if (CAUTARE_DATA_IMPRUMUT == a) {
+//          Search.searchBookByBorrowDate();
+        } else if (CAUTARE_POPULARITATE == a) {
+//          Search.searchByBorrowedTimes();
+        } else if (INAPOI_MENIU == a) {
+
+        }
+    }
+}
---
+++ src/main/java/library/SearchOptions.java
@@ -0,0 +1,14 @@
+package library;
+
+/**
+ * This class was created to make it easier to store the search options for the books
+ */
+
+public class SearchOptions {
+    public static final int CAUTARE_NUME = 1;
+    public static final int CAUTARE_AN = 2;
+    public static final int CAUTARE_AUTOR = 3;
+    public static final int CAUTARE_DATA_IMPRUMUT = 4;
+    public static final int CAUTARE_POPULARITATE = 5;
+    public static final int INAPOI_MENIU = 6;
+}
